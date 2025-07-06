ba92eb7a0e92a765bdbcafd54663ba2b2f93fa86
Cole Reilly <coltonmreilly@gmail.com>
2025-07-06 12:08:59 -0500

    import chromedriver

---
+++ scrape.py
@@ -0,0 +1,34 @@
+# build selenium scraper
+
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+# grab content from site
+
+def scrape_website(website):
+    print("Launching chrome browser...")
+
+    # specify chrome driver
+
+    chrome_driver_path = ""
+
+    # specify how driver should operate (headless, ignore images, etc.)
+
+    options = webdriver.ChromeOptions
+
+    # setup driver, specify service, pass options
+
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    # use webdriver to go to website
+    
+    try:
+        driver.get(website)
+        print("Page loaded...")
+        html = driver.page_source
+        time.sleep(10)
+
+        return html
+    finally:
+        driver.quit()
